4161d3622d1b9cb30daae986e3eb4ad681b18210
maxator-creator <77058926+maxator-creator@users.noreply.github.com>
2021-12-29 17:18:32 +0100

    Add files via upload

---
+++ maxime_29_12_afficher_plateau/main.c
@@ -0,0 +1,13 @@
+#include "Header.h"
+
+int main()
+{
+    t_joueur j1 = creation_joueur(1);
+    t_joueur* p_j1 = &j1;
+    j1.carte_sortie_prison = 1;
+    int de[2];
+    lance_de(de);
+    t_case plateau[TAILLE_PLATEAU];
+    creation_plateau(plateau);
+    afficher_plateau(plateau);
+}
