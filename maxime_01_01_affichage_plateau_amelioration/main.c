97f1506ac8ab522d8dd71f8c0721690be91b53fd
maxator-creator <77058926+maxator-creator@users.noreply.github.com>
2022-01-01 19:30:11 +0100

    Add files via upload

---
+++ maxime_01_01_affichage_plateau_amelioration/main.c
@@ -0,0 +1,14 @@
+#include "Header.h"
+
+int main()
+{
+    srand(time(0));
+    t_joueur tab_joueur[4];
+    creation_tab_joueur(tab_joueur);
+    t_banque banque = creation_banque();
+    t_banque* p_banque = &banque;
+    t_case plateau[TAILLE_PLATEAU];
+    creation_plateau(plateau);
+    tab_joueur[0].argent = 0;
+    tour(tab_joueur, plateau, p_banque);
+}
