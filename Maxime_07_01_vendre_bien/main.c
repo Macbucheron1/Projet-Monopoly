dfac33e853de81b0c5269818460a8c4acbd6cb77
maxator-creator <77058926+maxator-creator@users.noreply.github.com>
2022-01-07 22:51:07 +0100

    Add files via upload

---
+++ Maxime_07_01_vendre_bien/main.c
@@ -0,0 +1,24 @@
+#include "Header.h"
+
+int main()
+{
+    srand(time(0));
+    t_joueur tab_joueur[4];
+    creation_tab_joueur(tab_joueur);
+    t_banque banque = creation_banque();
+    t_banque* p_banque = &banque;
+    t_case plateau[TAILLE_PLATEAU];
+    creation_plateau(plateau);
+    plateau[6].num_joueur = 1;
+    plateau[6].nb_maison = 1;
+    printf("%d", tab_joueur[0].nb_propriete);
+    printf("le joueur 1 a: %d et le joueur 2 a %d\n", tab_joueur[0].argent, tab_joueur[1].argent);
+    tour(tab_joueur, plateau, p_banque);
+    printf("le joueur 1 a: %d et le joueur 2 a %d\n", tab_joueur[0].argent, tab_joueur[1].argent);
+    tour(tab_joueur, plateau, p_banque);
+    printf("le joueur 1 a: %d et le joueur 2 a %d\n", tab_joueur[0].argent, tab_joueur[1].argent);
+    tour(tab_joueur, plateau, p_banque);
+    printf("le joueur 1 a: %d et le joueur 2 a %d\n", tab_joueur[0].argent, tab_joueur[1].argent);
+    tour(tab_joueur, plateau, p_banque);
+    printf("le joueur 1 a: %d et le joueur 2 a %d\n", tab_joueur[0].argent, tab_joueur[1].argent);
+}
